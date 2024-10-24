7d8602fe80a46a52fed9b7d590de17216a71f6ac
adnan bezerra <adnanbezerra@proton.me>
2024-10-24 10:41:23 -0300

    feat/create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Route, Router } from "react-router-dom";
+import { Main } from "./pages/Main";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import ManageUser from "./pages/ManageUser";
+import Product from "./pages/Product";
+import Profile from "./pages/Profile";
+import Users from "./pages/Users";
+
 function App() {
-
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Router>
+                <Route path="/" component={<Main />} />
+                <Route path="/login" component={<Login />} />
+                <Route path="/forgot-password" component={<ForgotPassword />} />
+                <Route path="/manage/:id" component={<ManageUser />} />
+                <Route path="/product/:id" component={<Product />} />
+                <Route path="/profile/:id" component={<Profile />} />
+                <Route path="/users" component={<Users />} />
+            </Router>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
