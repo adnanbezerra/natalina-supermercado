98aeb9e49afe8ce105cdc5e1cf295ea9c55f69a4
adnan bezerra <adnanbezerra@proton.me>
2024-10-25 23:12:05 -0300

    refacc/use TS instead of JS on the project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import ManageUser from "./pages/ManageUser";
+import Product from "./pages/Product";
+import Profile from "./pages/Profile";
+import Users from "./pages/Users";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/manage/:id" element={<ManageUser />} />
+                <Route path="/product/:id" element={<Product />} />
+                <Route path="/profile/:id" element={<Profile />} />
+                <Route path="/users" element={<Users />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
