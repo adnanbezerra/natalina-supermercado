700e819f9eb2e68c87d94306f6fa66842ee265e1
adnan bezerra <adnanbezerra@proton.me>
2024-10-30 09:17:41 -0300

    refactor/accept className on components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,16 +5,23 @@ type ButtonProps = {
     onClick: (e: any) => void;
     disabled?: boolean;
     type?: "button" | "submit" | "reset";
+    className?: string;
 };
 
 export default function Button({
     children,
     onClick,
     disabled = false,
-    type = 'button',
+    type = "button",
+    className,
 }: ButtonProps) {
     return (
-        <button onClick={onClick} disabled={disabled} type={type} className='party-golden text-pine-green'>
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            type={type}
+            className={`party-golden text-pine-green ${className}`}
+        >
             {children}
         </button>
     );
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,13 +4,29 @@ interface InputProps {
     value: string;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     label: string;
+    placeholder?: string;
+    type?: string;
+    className?: string;
 }
 
-export default function Input({ value, onChange, label }: InputProps) {
+export default function Input({
+    value,
+    onChange,
+    label,
+    placeholder,
+    type,
+    className,
+}: InputProps) {
     return (
-        <div className='flex'>
+        <div className="flex">
             <label>{label}</label>
-            <input value={value} onChange={onChange} />
+            <input
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                type={type}
+                className={className}
+            />
         </div>
     );
 }
