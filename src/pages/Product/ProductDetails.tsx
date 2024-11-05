4b70b54c83f94d136d8ee13b9170cd290dcfc3e0
adnan bezerra <adnanbezerra@proton.me>
2024-11-05 09:29:12 -0300

    refactor/product services path

--- src/pages/Product/ProductDetails.tsx
+++ src/pages/Product/ProductDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchProduct } from '../../services/user/fetch-product';
+import { fetchProduct } from '../../services/product/fetch-product';
 import { IProduct } from '../../interfaces/product';
 
 const ProductPage: React.FC = () => {
